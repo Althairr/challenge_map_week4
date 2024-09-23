73e4b9f839945d824bfaa024b82d325ba697be66
Althairr <113684284+Althairr@users.noreply.github.com>
2024-09-23 22:16:31 +0700

    Update CafeDetailFragment.kt

--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +17,14 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CafeDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+private const val TAB_CONTENT = "TAB_CONTENT"
 class CafeDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val cafeDesc: TextView?
+        get() = view?.findViewById(R.id.cafe_desc)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,23 +42,37 @@ class CafeDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CafeDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CafeDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val cafeId = arguments?.getInt(CAFE_ID, 0) ?: 0
+        setCafeData(cafeId) // Use the passed cafeId from arguments
+    }
+
+
+    fun setCafeData(id: Int){
+        when(id){
+            CAFE_STARBUCKS -> {
+                cafeDesc?.text = getString(R.string.starbucks_desc)
             }
+            CAFE_JANJIJIWA -> {
+                cafeDesc?.text = getString(R.string.janjijiwa_desc)
+            }
+            CAFE_KOPIKENANGAN -> {
+                cafeDesc?.text = getString(R.string.kopikenangan_desc)
+            }
+        }
+    }
+
+    companion object {
+        private const val CAFE_ID = "CAFE_ID"
+        const val CAFE_STARBUCKS = 1
+        const val CAFE_JANJIJIWA = 2
+        const val CAFE_KOPIKENANGAN = 3
+
+        fun newInstance(cafeId: Int) = CafeDetailFragment().apply {
+            arguments = Bundle().apply {
+                putInt(CAFE_ID, cafeId)
+            }
+        }
     }
 }
